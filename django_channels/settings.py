0fbd9247e8937c2313b62b5e195bd858cfe14a30
David <davidmejia.05@@hotmail.com>
2022-05-18 09:11:46 -0500

    chat app

--- django_channels/settings.py
+++ django_channels/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'channels',
-    'integers'
+    'chat'
 ]
 
 MIDDLEWARE = [
--- django_channels/urls.py
+++ django_channels/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('integers.urls'))
+    path('chat/', include('chat.urls')),
 ]
