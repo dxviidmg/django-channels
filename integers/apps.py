5cc412dd5aa7aa95c2d6bafe8290783ca65a5f77
David <davidmejia.05@@hotmail.com>
2022-05-18 09:10:05 -0500

    delete numbers random

--- integers/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class IntegersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'integers'
--- integers/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    context = {
-        'text': 'Hello World',
-    }
-    return render(request, 'index.html', context=context)
